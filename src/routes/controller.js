13d60664e337287fa74d585af2a8bd98ec78ce47
Arminramazani1999 <144837949+Arminramazani1999@users.noreply.github.com>
2024-02-05 15:14:27 +0330

    fix contriller.js spelling to controller.js

--- src/routes/controller.js
+++ src/routes/controller.js
@@ -1,10 +1,10 @@
 const autoBindI = require("auto-bind-inheritance");
 const { validationResult } = require("express-validator");
-const User = require("./../models/user");
-const Category = require("./../models/category");
-const Product = require("./../models/product");
-const Order = require("./../models/order");
-const OrderItem = require("./../models/order_item");
+const User = require("../models/user");
+const Category = require("../models/category");
+const Product = require("../models/product");
+const Order = require("../models/order");
+const OrderItem = require("../models/order_item");
 
 class Controller {
   constructor() {
@@ -18,7 +18,7 @@ class Controller {
   validationBody(req, res) {
     const result = validationResult(req);
     if (!result.isEmpty()) {
-      const errors = result.array().map((err) => err.msg);  
+      const errors = result.array().map((err) => err.msg);
       res.status(400).json({
         message: "validation error",
         data: errors,
@@ -32,7 +32,7 @@ class Controller {
     next();
   }
 
-  respons({ res, message, code = 200, data = {} }) {
+  response({ res, message, code = 200, data = {} }) {
     res.status(code).json({
       message,
       data,
