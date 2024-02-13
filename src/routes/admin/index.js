21dfb1e931f95e350a3f7539e76530d95c49ff43
Arminramazani1999 <144837949+Arminramazani1999@users.noreply.github.com>
2024-02-13 20:20:55 +0330

    clean admin folder

--- src/routes/admin/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("./controller"); 
-
-router.get("/", controller.dashboard);
-
-module.exports = router;
